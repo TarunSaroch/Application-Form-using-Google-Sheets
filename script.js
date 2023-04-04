6e1afe9e2c30016668e0e2a9d9f2be803a127318
Tarun Saroch <123083325+TarunSaroch@users.noreply.github.com>
2023-04-04 23:37:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ const form = document.querySelector('form');
 const submitBtn = document.getElementById('sub');
 
 submitBtn.addEventListener('click', (event) => {
+  
   event.preventDefault(); // Prevent form submission
+  
 });
